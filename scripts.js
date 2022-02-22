e188f966e9532ae04a36d49ded800cc205ac1ee1
Alex LeFevre <alexander.m.lefevre@gmail.com>
2022-02-21 20:38:09 -0500

    working tobintracker

--- scripts.js
+++ scripts.js
@@ -1,16 +1,40 @@
-// is this best approach of can I look back to HC projects and write a simple server that we can update as part of this page
+// setting wins on page load
+let setWins = Number(localStorage.getItem('Wins'));
+document.getElementById('wins').value = setWins;
 
-// set all inputs to what is stored in local storage
+// setting losses on page load
+let setLosses = Number(localStorage.getItem('Losses'));
+document.getElementById('losses').value = setLosses;
 
-// set the onchange=storeData() for each input on page load
-let inputs = document.getElementsByTagName('input');
-for (var i = 0; i < inputs.length; i++) {
-  inputs.addEventListener('change', storeData);
+// setting pushes on page load
+let setPushes = Number(localStorage.getItem('Pushes'));
+document.getElementById('pushes').value = setPushes;
+
+// Storing wins in local
+function storeWins() {
+  let getWins = document.getElementById('wins').value;
+  localStorage.setItem('Wins', getWins);
 }
 
-// stores value of first input
-// need to update so it does all of the values
-function storeData() {
-  let input = document.getElementById('test').value;
-  localStorage.setItem('test', input);
+// Storing losses in local
+function storeLosses() {
+  let getLosses = document.getElementById('losses').value;
+  localStorage.setItem('Losses', getLosses);
+}
+
+// Storing pushes in local
+function storePushes() {
+  let getPushes = document.getElementById('pushes').value;
+  localStorage.setItem('Pushes', getPushes);
+}
+
+// calculates the win percentage on page load. Find way to calc every time a # changes
+let totalWagers = setWins + setLosses;
+let winPercentge = ((setWins / totalWagers) * 100).toFixed(2);
+document.getElementById('winPercentage').innerHTML = winPercentge + '%';
+
+if (winPercentge >= 50) {
+  document.getElementById('winPercentage').style.color = 'green';
+} else {
+  document.getElementById('winPercentage').style.color = 'red';
 }
