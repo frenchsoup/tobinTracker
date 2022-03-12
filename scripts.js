1206f18a0a5a86a0ecea8079ad27fda6acb414d1
Alex LeFevre <alexander.m.lefevre@gmail.com>
2022-03-12 08:10:45 -0500

    auto calc

--- scripts.js
+++ scripts.js
@@ -10,33 +10,78 @@ document.getElementById('losses').value = setLosses;
 let setPushes = Number(localStorage.getItem('Pushes'));
 document.getElementById('pushes').value = setPushes;
 
-// storing wins in local
+// setting unit on page load
+let setUnit = Number(localStorage.getItem('unitCount'));
+document.getElementById('unitCount').value = setUnit;
+
+// setting values on page load
+calcWinPercentage();
+calcUnitWinLoss();
+
+// storing wins in local and calculating
 function storeWins() {
-  let getWins = document.getElementById('wins').value;
+  let getWins = Number(document.getElementById('wins').value);
   localStorage.setItem('Wins', getWins);
+  calcWinPercentage();
+  calcUnitWinLoss();
 }
 
-// storing losses in local
+// storing losses in local and calculating
 function storeLosses() {
-  let getLosses = document.getElementById('losses').value;
+  let getLosses = Number(document.getElementById('losses').value);
   localStorage.setItem('Losses', getLosses);
+  calcWinPercentage();
+  calcUnitWinLoss();
 }
 
-// storing pushes in local
+// storing pushes in local and calculating
 function storePushes() {
-  let getPushes = document.getElementById('pushes').value;
+  let getPushes = Number(document.getElementById('pushes').value);
   localStorage.setItem('Pushes', getPushes);
+  calcWinPercentage();
+  calcUnitWinLoss();
 }
 
-let totalWagers = setWins + setLosses;
-let winPercentge = ((setWins / totalWagers) * 100).toFixed(2);
-document.getElementById('winPercentage').innerHTML = winPercentge + '%';
-
-if (winPercentge >= 50) {
-  document.getElementById('winPercentage').style.color = 'green';
-} else {
-  document.getElementById('winPercentage').style.color = 'red';
+// storing unit in local and calculating
+function storeUnit() {
+  let getUnit = Number(document.getElementById('amount').value);
+  localStorage.setItem('Unit', getUnit);
+  calcUnitWinLoss();
 }
+
+// calculate win percentage on change of any of the numbers
+function calcWinPercentage() {
+  let getWins = Number(document.getElementById('wins').value);
+  let getLosses = Number(document.getElementById('losses').value);
+  let totalWagers = getWins + getLosses;
+  let winPercentage = ((getWins / totalWagers) * 100).toFixed(2);
+
+  if (winPercentage >= 50) {
+    document.getElementById('winPercentage').style.color = 'green';
+  } else {
+    document.getElementById('winPercentage').style.color = 'red';
+  }
+  document.getElementById('winPercentage').innerHTML = winPercentage + '%';
+}
+
+// calculating profit/loss based on unit amount
+function calcUnitWinLoss() {
+  let getWins = Number(document.getElementById('wins').value);
+  let getLosses = Number(document.getElementById('losses').value);
+  let amount = document.getElementById('amount').value;
+
+  let amountWon = getWins * (amount * 0.9);
+  let amountLost = getLosses * amount;
+
+  let unitWinLoss = (amountWon - amountLost).toFixed(2);
+  if (unitWinLoss >= 0) {
+    document.getElementById('unitCount').style.color = 'green';
+  } else {
+    document.getElementById('unitCount').style.color = 'red';
+  }
+  document.getElementById('unitCount').innerHTML = '$' + unitWinLoss;
+}
+
 //TODO:
 // calculates the win percentage on page load. Find way to calc every time a # changes
 // get $ calcs added in
